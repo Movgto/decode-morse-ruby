8161194021c2361c180320325d7cd2005039be27
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-10-04 23:17:46 +0100

    create a method to decode a Morse code character

--- decode_morse.rb
+++ decode_morse.rb
@@ -9,3 +9,19 @@ def decode_char(morse_code)
   }
   morse_code_dict[morse_code]
 end
+
+def decode_word(morse_word)
+  characters = morse_word.split
+  characters.map { |char| decode_char(char) }.join
+end
+
+def decode_message(morse_message)
+  words = morse_message.split('   ')
+  words.map { |word| decode_word(word) }.join(' ')
+end
+
+message = '--  -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+
+decoded_message = decode_message(message)
+
+puts decoded_message
